4fbc3ee4bea812ec3fca735d115be8348e5c3dcc
Junha Park <poiu7825@pusan.ac.kr>
2025-10-31 02:07:24 +0900

    feat: 로또 번호 범위 유효성 검사 로직 추가 및 크기 검사 메소드 이름 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,18 +1,35 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        int start = numbers.getFirst();
+        int end = numbers.getLast();
+        if (start < 1 || end > 45 ) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45까지의 숫자로 이루어져야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if(numbers.size() != new HashSet<>(numbers).size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되어선 안됩니다.");
         }
     }
 
