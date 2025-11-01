50f36ef58dd48919f291022ca67a8cf92c6b9c2b
Junha Park <poiu7825@pusan.ac.kr>
2025-11-01 16:34:46 +0900

    feat: Application에 시작 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.InputHandler;
+import lotto.controller.LottoController;
+import lotto.util.LottoGenerator;
+import lotto.util.RandomNumberGenerator;
+import lotto.view.ConsoleInputView;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
+        InputHandler inputHandler = new InputHandler(inputView);
+        RandomNumberGenerator randomNumberGenerator = new LottoGenerator();
+        OutputView outputView = new OutputView();
+
+        LottoController lottoController = new LottoController(inputHandler, randomNumberGenerator, outputView);
+        lottoController.run();
     }
 }
