26786556520b5f0f97953b118c5d9529dfb0b7e9
Junha Park <poiu7825@pusan.ac.kr>
2025-11-02 18:01:02 +0900

    refactor: 출력 형식 고정을 위한 메서드 추가

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -5,21 +5,23 @@ import java.util.EnumMap;
 
 public enum Prize {
 
-    FIRST_PRIZE(6, 2_000_000_000, false),
-    SECOND_PRIZE(5, 30_000_000, true),
-    THRID_PRIZE(5, 1_500_000, false),
-    FOURTH_PRIZE(4, 50_000, false),
-    FIFTH_PRIZE(3, 5_000, false),
-    MISS(0, 0, false);
+    FIRST_PRIZE(6, 2_000_000_000, false, "6개 일치"),
+    SECOND_PRIZE(5, 30_000_000, true, "5개 일치, 보너스 볼 일치"),
+    THRID_PRIZE(5, 1_500_000, false, "5개 일치"),
+    FOURTH_PRIZE(4, 50_000, false, "4개 일치"),
+    FIFTH_PRIZE(3, 5_000, false, "3개 일치"),
+    MISS(0, 0, false, "낙첨");
 
     private final Integer matchCount;
     private final Integer prizeMoney;
     private final boolean isBonusMatch;
+    private final String description;
 
-    Prize(final Integer matchCount, final Integer prizeMoney, final boolean isBonusMatch) {
+    Prize(final Integer matchCount, final Integer prizeMoney, final boolean isBonusMatch, final String description) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
         this.isBonusMatch = isBonusMatch;
+        this.description = description;
     }
 
     public static Prize find(Integer matchCount, boolean isBonusMatch) {
@@ -36,5 +38,9 @@ public enum Prize {
         return prizes.entrySet().stream()
                 .mapToInt(entry -> entry.getKey().prizeMoney * entry.getValue())
                 .sum();
+    }
+
+    public String getStatisticsMessage() {
+        return String.format("%s (%,d원)", this.description, this.prizeMoney);
     }
 }
