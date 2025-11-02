0d1f22827efef3e61c4772885b960e40b834941c
Junha Park <poiu7825@pusan.ac.kr>
2025-11-02 16:58:25 +0900

    refactor: 매직 넘버 담당 클래스 추가

---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+public final class Constants {
+
+    private Constants() {}
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+
+    public static final int LOTTO_PRICE = 1_000;
+}
