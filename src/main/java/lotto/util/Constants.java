a77aa3588f4bb6573ee2f96d23b2aecfb442cf85
Junha Park <poiu7825@pusan.ac.kr>
2025-11-02 17:05:13 +0900

    refactor: 구분자 쉼표(,)를 Constants 클래스에 추가

--- src/main/java/lotto/util/Constants.java
+++ src/main/java/lotto/util/Constants.java
@@ -7,6 +7,7 @@ public final class Constants {
     public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
-
     public static final int LOTTO_PRICE = 1_000;
+
+    public static final String DEFAULT_DELIMITER = ",";
 }
